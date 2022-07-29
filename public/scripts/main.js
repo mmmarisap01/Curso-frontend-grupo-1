8ac4141744e875adad7b5cc76222428caa3c29ac
Marisa Posada <marisa.posada@gmail.com>
2022-07-29 17:44:21 +0200

    api2

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,8 +4,6 @@ import { AlertFechaHoy } from "./modules/AlertFechaHoy.js"
 AlertFechaHoy();
 
 
-
-
 import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
 //import { KeynavController } from "./keynav_controller.js"
 import { LogController as KeynavController } from "./log_controller.js"
@@ -16,8 +14,3 @@ const app = Application.start()
 app.register("kn", KeynavController)
 
 
-async function getMyData() {
-    let response = await fetch("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}");
-    let data = await response.json();
-    console.log(data)
-   }
